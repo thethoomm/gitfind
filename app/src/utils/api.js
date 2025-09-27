edf173c3dde883c6b3d8813e190d49bde743d252
thethoomm <thomas.santos02209@gmail.com>
2025-09-27 08:52:37 -0300

    exibição de dados no frontend inicial

---
+++ app/src/utils/api.js
@@ -0,0 +1,15 @@
+const GITFIND_API_URL = import.meta.env.VITE_GITFIND_API_URL;
+
+export async function findProfiles(username) {
+  try {
+    const response = await fetch(`${GITFIND_API_URL}/search?q=${username}`, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    return data.profiles;
+  } catch (error) {
+    console.error("Erro na requisição á API Gitfind", error);
+  }
+}
